e386b8b2a24ac4e3291649530c756b56f4247c2c
Souji Kenzaki <chenuki@yahoo.co.jp>
2018-02-24 22:39:59 +0900

    個別配布の為のプラグイン化

--- Knzk_SceneFreeze_patch.js
+++ Knzk_SceneFreeze_patch.js
@@ -1,3 +1,37 @@
+//=============================================================================
+// Knzk_SceneFreeze_patch.js
+//=============================================================================
+/*:
+ * @plugindesc Queue up the changeScene Requests and execute them in order
+ * @author Souji Kenzaki
+ *
+ * 
+ *
+ * @help 
+ * This is a response to the MV 1.6.0 patch.
+ * As the patch did not resolve the root cause and caused a lot of side effect, this is an attempt to resolve the root cause 
+ * of "What will happen when scene change happens before previous one resolves?"
+ * 
+ * When you introduce this patch, it will cause those scene change requests to be queued until previous one resolved.
+ * As this somewhat changes how the scene change is processed, please use with care. 
+ */
+
+/*:ja
+ * @plugindesc シーン変更が多重で呼ばれた際、それを順番に実行できるようにする
+ * @author 剣崎宗二
+ *
+ *
+ * @help 
+ * MV 1.6.0への修正、及び新規機能の追加パッチとなります。
+ * 該当のバージョンはフリーズバグの根本的な原因を修正せず、尚且つ新たな問題を差し込んだため、このパッチはそれを是正します。
+ * 
+ * 該当のフリーズバグの根本的な原因は「前のシーン遷移が完了しないまま次のシーン遷移が発生した際、どうなるか？」
+ * ですので、このパッチはその際「キューにシーン遷移リクエストを累積し、順番に（前のシーンを正しく終了させた後）実行する」と言う手段を取っております。
+ * 根本的にシーン遷移の仕組みを変更する為、
+ * 使用時は十分にご注意ください。
+ */
+
+(function(){
 SceneManager._loadingQueue            = [];
 SceneManager._sceneStopped            = false;
 
@@ -79,3 +113,5 @@ Scene_Item.prototype.update = function() {
     //     this._categoryWindow.activate();
     // }
 };
+
+})();
